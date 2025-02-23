096765d729b7ab76be8ff6f62fa1e6b57a0b56f1
ArynaPatlakh <patlakhap@gmail.com>
2025-02-23 16:03:34 +0000

    sorted

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -12,12 +12,17 @@ import {
   updateCotactSchame,
 } from '../validation/contacts.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContacts = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
   const contacts = await getAllContacts({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
 
   res.status(200).json({

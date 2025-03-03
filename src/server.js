2e73562857649e1f1edca9ce0af6782c6536cace
ArynaPatlakh <patlakhap@gmail.com>
2025-03-03 19:11:03 +0000

    done 2

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFound } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import router from './routers/index.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -39,6 +40,8 @@ const setupServer = () => {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFound);
 
